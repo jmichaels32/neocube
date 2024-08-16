e2ead9063ce493029aebaf997d29cd9b8d767bae
Jack Michaels <jackfm@stanford.edu>
2024-08-15 22:43:19 -0400

    Color Functionality

--- cube.c
+++ cube.c
@@ -8,7 +8,7 @@
 #include "lib/util.h"
 
 // Settings to change the way the cube animates
-float theta_increment = 0.03, phi_increment = 0.03, alpha_increment = 0.02;
+float theta_increment = 0.04, phi_increment = 0.03, alpha_increment = 0.02;
 float cube_x = 1, cube_y = 0;
 int width = 20;
 int camera_distance = 100;
@@ -16,6 +16,7 @@ int camera_distance = 100;
 // Global variables required for functionality
 char *ascii_array;
 float *depth_array;
+int *color_array;
 int screen_width, screen_height;
 float theta = 0, phi = 0, alpha = 0;
 
@@ -43,7 +44,7 @@ float threeD_z(int x, int y, int z) {
 }
 
 // Store the projected (2 dimensional) coordinates from three dimensional space
-void twoD_points(int x, int y, int z, char ascii) {
+void twoD_points(int x, int y, int z, char ascii, int color) {
     float threeDpoint_x = threeD_x(x, y, z);
     float threeDpoint_y = threeD_y(x, y, z);
     float threeDpoint_z = threeD_z(x, y, z) + camera_distance;
@@ -59,6 +60,7 @@ void twoD_points(int x, int y, int z, char ascii) {
         if (scaling_factor > depth_array[index]) {
             depth_array[index] = scaling_factor;
             ascii_array[index] = ascii;
+            color_array[index] = color;
         }
     }
 }
@@ -72,6 +74,7 @@ int main() {
     // Prepare arrays
     ascii_array = (char*)malloc(screen_width * screen_height * sizeof(char));
     depth_array = (float*)malloc(screen_width * screen_height * sizeof(float));
+    color_array = (int*)malloc(screen_width * screen_height * sizeof(int));
 
     // Make sure malloc allocated correctly
     if (ascii_array == NULL || depth_array == NULL) {
@@ -87,16 +90,22 @@ int main() {
         // Clear memory
         memset(ascii_array, ' ', screen_width * screen_height);
         memset(depth_array, 0, screen_width * screen_height * 4);
+        memset(color_array, 7, screen_width * screen_height * 4);
 
+        //    _
+        //  _|2|___
+        // |1|3|5|6|
+        //  ‾|4|‾‾‾
+        //    ‾
         // Calculate iteration of graphics
         for (float x = -width; x < width; x += 0.3) {
             for (float y = -width; y < width; y += 0.3) {
-                twoD_points(x, y, -width, '#');
-                twoD_points(width, y, x, '-');
-                twoD_points(-width, y, -x, '/');
-                twoD_points(-x, y, width, ';');
-                twoD_points(x, -width, -y, '%');
-                twoD_points(x, width, y, '?');
+                twoD_points(x, y, -width, '#', 4);   // 1
+                twoD_points(x, width, y, '`', 4);    // 2
+                twoD_points(-width, y, -x, '*', 6);  // 3
+                twoD_points(x, -width, -y, '.', 4);  // 4
+                twoD_points(-x, y, width, ';', 4);   // 5
+                twoD_points(width, y, x, '-', 6);    // 6
             }
         } 
 
@@ -104,6 +113,7 @@ int main() {
         printf("\x1b[2H");
         for (int index = 0; index < screen_width * screen_height; ++index) {
             if (index % screen_width != 0) {
+                printf("\033[1;3%dm", color_array[index]);
                 putchar(ascii_array[index]);
             } else {
                 putchar(10);
@@ -119,6 +129,7 @@ int main() {
     // Clean memory up
     free(ascii_array);
     free(depth_array);
+    free(color_array);
     return 0;
 }
 
